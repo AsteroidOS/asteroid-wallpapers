6ae690deb45c95cdf1fa94ec96b567b50493aa19
eLtMosen <koennecke@mosushi.com>
2022-01-09 18:46:12 +0100

    Add script to simplify preview image generation

---
+++ generate-scaled-images.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Scale and compress all jpg, jpeg, png, svg, bmp and webp images to existing preview resolution folders
+
+if ! command -v magick &> /dev/null
+then
+    echo "
+$(tput setaf 1)Image Magick could not be found. Install magick from your package manager.$(tput sgr0)"
+    exit
+else
+    echo "
+$(tput setaf 2)magick found, proceeding...$(tput sgr0)
+         "
+fi
+
+unset options i
+while read -r -d $'\0' dir
+do
+  if [ ! $dir = "full/" ]
+  then
+    options[i++]="$dir"
+  fi
+done < <(find */ -maxdepth 0 -type d -print0 )
+
+for file in *.{jpg,jpeg,png,svg,bmp,webp}; do
+  if [ ! -f $file ]
+  then
+    echo "$(tput setaf 214)No $file files present.$(tput sgr0)"
+  else
+    mogrify -resize 480x480 -quality 80 -format jpg -path full $file
+    echo "480px full wallpaper generated for $file."
+    for opt in "${options[@]}"
+    do
+      mogrify -resize ${opt::-1}x${opt::-1} -quality 55 -format jpg -path ${opt::-1} $file
+      echo "${opt::-1}px preview generated for $file."
+    done
+  fi
+  echo "
+$(tput setaf 2)All scaled and compressed images for $file generated succesfully.$(tput sgr0)
+       "
+done
